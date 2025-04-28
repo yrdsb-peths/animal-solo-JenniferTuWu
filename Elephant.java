e11800cbc8c19821d6dd639f61d0296b6873109e
JenniferTuWu <jenniferydt@gmail.com>
2025-04-28 09:01:47 -0400

    set up. Hero and food

---
+++ Elephant.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Elephant, our hero
+ * 
+ * @author Tu
+ * @version April 2025
+ */
+public class Elephant extends Actor
+{    
+    public void act()
+    {
+        move(1);
+    }
+}
