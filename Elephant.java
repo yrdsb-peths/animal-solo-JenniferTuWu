78b99b5bf082e82b4dee0a106cc293329112a99b
JenniferTuWu <jenniferydt@gmail.com>
2025-04-28 09:11:36 -0400

    set images

--- Elephant.java
+++ Elephant.java
@@ -8,6 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {    
+    public Elephant(){
+        setImage("images/elephant.png");
+    }
+    
     public void act()
     {
         move(1);
