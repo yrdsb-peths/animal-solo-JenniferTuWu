8581ba737c2e92e39902b12f8c1d024d9a74611c
JenniferTuWu <jenniferydt@gmail.com>
2025-05-11 16:58:50 -0400

    Add another world and add title

---
+++ TitleScreen.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author Tu 
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Elephant", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+        
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
+    }
+}
